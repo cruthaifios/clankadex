db4fa675710006db47c8cc97df5084906b97054e
Cruthaifios <curthaifios@gmail.com>
2026-03-06 16:53:22 -0600

    Don't commit dist

--- dist/main/web.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const SERVER_PORT = Number(process.env.PORT) || 19321;
-(0, server_1.startServer)(SERVER_PORT).then(() => {
-    console.log(`Clankadex web app running at http://localhost:${SERVER_PORT}`);
-});
